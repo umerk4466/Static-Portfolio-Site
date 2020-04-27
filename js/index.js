27aff0cff7884f245bfa1454223ff70fcab843c5
umerk4466 <umerk4466@gmail.com>
2020-04-27 18:08:01 +0100

    full side added

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// show animation until page is not loaded
+$(window).on("load", function() {
+    $(".loader").fadeOut();
+});
+
+// scroll down smooth buttom
+$(document).ready(function() {
+    $(".custom-button").click(function() {
+        $("html, body").animate({ scrollTop: $("#about_me_page").offset().top + 10 },
+            "slow"
+        );
+    });
+});
+
+// Add smooth scrolling on all links inside the navbar
+$(document).ready(function() {
+    $(".navbar-nav a").on("click", function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate({
+                    scrollTop: $(hash).offset().top + 5
+                },
+                800
+                //   function() {
+                //     // Add hash (#) to URL when done scrolling (default click behavior)
+                //     window.location.hash = hash;
+                //   }
+            );
+        } // End if
+    });
+});
+
+// set percentage with animaton on scroll
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+        var position = $("#skill_page").offset().top;
+        if ($("body").scrollTop() >= position - 300) {
+            // set percentage of the skill from their given value
+            $(".skill-percentage").each(function() {
+                var percentage = $(this).text();
+                $(this).animate({ width: parseInt(percentage) + "%" }, 3000);
+            });
+        }
+    });
+});
